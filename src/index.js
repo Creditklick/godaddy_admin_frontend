a4c5f8a2a0f2d6a3e89bee94b6c78ca1fcfae2fb
Creditklick <127750700+Creditklick@users.noreply.github.com>
2025-02-14 17:28:05 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ClerkProvider } from '@clerk/clerk-react';
 import { BrowserRouter } from "react-router-dom";
-const PUBLISHABLE_KEY = process.env.REACT_APP_CLERK_PUBLISHABLE_KEY;
+//const PUBLISHABLE_KEY = process.env.REACT_APP_CLERK_PUBLISHABLE_KEY;
+const PUBLISHABLE_KEY = 'pk_test_aG9uZXN0LWR1Y2stODAuY2xlcmsuYWNjb3VudHMuZGV2JA';
 
 if (!PUBLISHABLE_KEY) {
   throw new Error("Missing Clerk Publishable Key! Check your .env file.");
